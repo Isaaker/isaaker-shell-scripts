7a27371905f88c1ddaaa745a137815155f67456e
Isaac Hernán <77550577+Isaaker@users.noreply.github.com>
2023-08-16 10:53:03 +0200

    Create script.sh

---
+++ nginx_access_log/script.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+# Created by: Isaaker
+# Read more: https://github.com/Isaaker/isaaker-shell-scripts/
+# License: Creative Commons Attribution-ShareAlike 4.0 International Public License
+
+
+# View headers
+echo "------------------------------------------"
+echo "| IP | User | Time | Body of the request |"
+echo "------------------------------------------"
+
