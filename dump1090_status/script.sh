cbb66879d3ff0f1ddd562842408c026b57ff6f39
Isaac Hernán <77550577+Isaaker@users.noreply.github.com>
2023-12-31 15:04:14 +0100

    Update script.sh

--- dump1090_status/script.sh
+++ dump1090_status/script.sh
@@ -4,13 +4,13 @@
 # License: https://github.com/Isaaker/isaaker-shell-scripts/blob/main/LICENSE.txt
 # RUN AS SUDO
 
-# Check Aircraft files exist
+# check aircraft file exist
 if [ ! -f /run/dump1090-fa/aircraft.json ]; then
 #Wall
 wall "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 stopped running, restarting services"
 
 #Log
-sudo echo "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 stopped running" > /var/log/dump1090_restart_log.txt
+sudo echo "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 stopped running" >> /var/log/dump1090_restart_log.txt
 
 #Restarting Services
 systemctl restart dump1090-fa.service
@@ -21,22 +21,23 @@ systemctl restart piaware.service
 exit 1
 fi
 
-# Check Aircraft Attribute
+# Check .aircraft contains current tracked aircrafts
 if jq '.aircraft | length > 0' /run/dump1090-fa/aircraft.json; then
 #Wall
 wall "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 still running"
 
 #Log
-sudo echo "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 still running" > /var/log/dump1090_restart_log.txt
+sudo echo "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 still running" >> /var/log/dump1090_restart_log.txt
 
 exit 0
+
 else
 
 #Wall
 wall "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 stopped running, restarting services"
 
 #Log
-sudo echo "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 stopped running" > /var/log/dump1090_restart_log.txt
+sudo echo "[$(date +"%d-%m-%Y %H:%M:%S")] Dump1090 stopped running" >> /var/log/dump1090_restart_log.txt
 
 #Restarting Services
 systemctl restart dump1090-fa.service
